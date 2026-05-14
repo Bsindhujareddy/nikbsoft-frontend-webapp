93428b9fc1054d48c69c7a4808865321075b0d14
Bsindhujareddy <sindhuja94b@gmail.com>
2026-05-14 10:54:05 +0530

    fc

--- frontend/scripts/install_dependencies.sh
+++ frontend/scripts/install_dependencies.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-echo "Installing frontend..."
+sudo chown -R ubuntu:ubuntu /home/ubuntu/frontend-app
 
-cd /home/ubuntu/frontend
+cd /home/ubuntu/frontend-app
+
+rm -rf node_modules
+rm -f package-lock.json
+
+npm cache clean --force
 
 npm install
-npm run build
